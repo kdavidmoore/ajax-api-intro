3aa2bbd59459131549c85c080318ef12dd3ce4f6
Keith Moore <keith.m4@gmail.com>
2016-03-24 20:10:53 -0400

    USGS REST IV Service Data
    Displays most recent data from all USGS-monitored water bodies in
    Georiga. Uses footables (DataTables abandoned). Need to add search
    function and styling.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Demonstration of AXAJ and Footables
+# Streams of Georgia
 
-## Class project for March 22, 2016
+## Personal project of March 2016
 
-This page uses an AJAX request in jQuery to get stocks data from Yahoo's API; then it displays the stocks data using footables.
+Using AJAX (jQuery's $.getJSON method), this web page pulls data from the [USGS REST IV service](http://waterservices.usgs.gov/rest/) and displays the most recent data from each monitored water body in the state of Georgia. 
 
-[I learned this at DigitalCrafts](https://www.digitalcrafts.com)
+### Note: you will get a CORS error if you try to use this without a web server set up (or CORS-enabling browser extension).
--- scripts.js
+++ scripts.js
@@ -1,68 +1,54 @@
 $(document).ready(function(){
-	$("#le-table").DataTable( {
-		"ajax": "test.json",
-		"columns": [
-			{ "value.timeSeries":"sourceInfo.siteName" },
-			{ "value.timeSeries":"sourceInfo.siteName" },
-			{ "value.timeSeries":"sourceInfo.siteName" },
-			{ "value.timeSeries":"sourceInfo.siteName" },
-			{ "value.timeSeries":"sourceInfo.siteName" }
-		]
-	});
+	
+	var ivBaseURL = 'http://waterservices.usgs.gov/nwis/iv/?format=json';
+	var searchBy = '&stateCd=ga';
+	var searchParams = '&parameterCd=00060,00065';
+	var fullURL = ivBaseURL + searchBy + searchParams;
 
-	// add a submit handler
-	/* $('#search').submit(function(){
-		var searchSym = $('#symbol').val();
+	var sites = [];
+	var varDescriptions = [];
+	$.getJSON(fullURL, function(waterData) {
+		console.log(waterData.value.timeSeries);
 
-		// url for Yahoo's API
-		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("' + searchSym + '")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+		var newHTML = '';
+		$(waterData.value.timeSeries).each(function(){
+			var thisSiteName = this.sourceInfo.siteName;
+			var thisSiteCode = this.sourceInfo.siteCode.value;
+			var thisSiteLat = this.sourceInfo.geoLocation.geogLocation.latitude;
+			var thisSiteLong = this.sourceInfo.geoLocation.geogLocation.latitude;
+			var thisVarDescription = this.variable.variableDescription;
+			var thisValue = this.values[0].value[0].value;
+			var thisSampleTime = this.values[0].value[0].dateTime;
 
-		// url for USGS IV REST service
-		// var url2 = 'http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=GA&startDT=2015-11-20&endDT=2016-03-20&parameterCd=00060,00065,00010,00076';
+			// saving these for later, just in case
+			sites[thisSiteCode] = thisSiteName;
+			varDescriptions.push(thisVarDescription);
 
-		// url for USGS DV REST service
-		// var url3 = '';
+			// write the table
+			newHTML += '<tr>';
+			newHTML += updateTable(thisSampleTime);
+			newHTML += updateTable(thisSiteName);
+			newHTML += updateTable(thisSiteLat);
+			newHTML += updateTable(thisSiteLong);
+			newHTML += updateTable(thisVarDescription);
+			newHTML += updateTable(thisValue);
+			newHTML += '</tr>'
+		});
 
-		// AJAX call - JS goes to url and brings back yahooResult
-		$.getJSON(url, function(yahooResult){
-			console.log(yahooResult);
-			var stockInfo = yahooResult.query.results.quote;
-			var newHTML;
-			if (yahooResult.query.count > 1) {
-				for (i=0; i<yahooResult.query.count; i++) {
-					newHTML += updateTable(stockInfo[i]);
-				}
-			} else {
-				newHTML = updateTable(stockInfo);
-			}
-			$('#ticker-body').html(newHTML);			
-			addSpecialClasses();
-			});
-		event.preventDefault();
+		// just kidding, actually write the table
+		$('#table-body').html(newHTML);
+		// initialize footables
+		getFootables();
 	});
 
 	function updateTable(thingToLoopThrough) {
-		var newHTMLRow = '<tr><td>' + thingToLoopThrough.Symbol + '</td>';
-		newHTMLRow += '<td>' + thingToLoopThrough.Name + '</td>';
-		newHTMLRow += '<td>' + thingToLoopThrough.Ask + '</td>';
-		newHTMLRow += '<td class="daily-change">' + thingToLoopThrough.Change + '</td>';
-		newHTMLRow += '<td>' + thingToLoopThrough.DaysHigh + '</td></tr>';
-		return newHTMLRow;
+		var newTD = '<td>' + thingToLoopThrough + '</td>';
+		return newTD;
 	}
 
-	function addSpecialClasses() {
-		// if dailychange is negative, turn text red; if positive, turn it green
-		$('.daily-change').each(function() {
-			var dailyChange = $(this).html();
-			if (dailyChange.indexOf('+') > -1) {
-				$(this).addClass('green');
-				console.log('Turn it green!');
-			} else if (dailyChange.indexOf('-') > -1) {
-				$(this).addClass('red');
-				console.log('Turn it red!');
-			}
+	function getFootables() {
+		$(function() {
+			$('.footable').footable();
 		});
-		// add other classes...if asking price > dayshigh....
-	} */
-
+	}
 });
